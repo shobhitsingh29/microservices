5bf06a2004a6e886fa6b670f8733ed7b8337181b
shobhit singh <shobhitsingh29@gmail.com>
2025-07-10 08:08:21 +0200

    retry+circuit breaker

--- UserService/src/main/java/com/lcwd/user/service/controller/UserController.java
+++ UserService/src/main/java/com/lcwd/user/service/controller/UserController.java
@@ -3,10 +3,14 @@ package com.lcwd.user.service.controller;
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.exceptions.ResourceNotFoundException;
 import com.lcwd.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -14,22 +18,40 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        User user1= userService.saveUser(user);
+        User user1 = userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+    int retryCount = 1;
+
+     @GetMapping("/{userId}")
+    // @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+    public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
+        logger.info("Getting user with id: " + userId);
+        logger.info("Retry count: " + retryCount++);
         User user = userService.getUser(userId);
         if (user == null) {
             throw new ResourceNotFoundException("User not found with id: " + userId);
         }
         return ResponseEntity.ok(user);
+    }
+
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+      //  logger.info("Fallback method called for getSingleUser due to: {}", ex.getMessage());
+        User user = new User();
+        user.setUserId(userId);
+        user.setName("Default User");
+        user.setEmail("dummy@gmail.com");
+        user.setAbout("This is a fallback user due to an error in fetching the actual user data.");
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
     @GetMapping
